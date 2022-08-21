600e540fed2855000b99f8be31bfd9f2e167b225
Rodrigo Henrique <rodhenrc@outlook.com>
2022-08-21 15:51:49 -0300

    Adição das imagens, mudança de fonte, adição do axios, mudanças no CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,133 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import pokerCard from "./images/pokerCard.svg";
+import axios from "axios";
 import "./App.css";
+
+interface DeckTypes {
+  deckId: string;
+}
+
+interface CardType {
+  code: string;
+  image: string;
+  value: number;
+}
 
 function App() {
   const [gameOver, setGameOver] = useState(false);
-  const [sortedCards, setSortedCards] = useState<number[]>([]);
+  const [sortedCards, setSortedCards] = useState<CardType[]>([]);
   const [finalScore, setFinalScore] = useState(0);
+  const [deck, setDeck] = useState("");
+
+  useEffect(() => {
+    const getInitialDeck = async () => {
+      const deckRequest = await axios.post("http://localhost:8080/deck");
+      const deck: DeckTypes = deckRequest.data;
+      setDeck(deck.deckId);
+    };
+
+    getInitialDeck();
+  }, [setDeck]);
 
   const reduceFunc = (total: number, current: number) => {
     return total + current;
   };
 
-  const generateNewCard = () => {
+  const generateNewCard = async () => {
     if (gameOver) return;
 
-    const randNum = Math.ceil(Math.random() * 13);
-    let sum;
+    const cardRequest = await axios.get(`http://localhost:8080/card/${deck}`);
 
-    if (sortedCards.length > 0) {
-      sum = sortedCards.reduce(reduceFunc) + randNum;
-    } else {
-      sum = randNum;
-    }
+    const newCard: CardType = cardRequest.data.card;
 
-    setSortedCards((prev) => {
-      return [...prev, randNum];
+    const newState = [...sortedCards, newCard];
+
+    const cardsValue = newState.map((i) => {
+      return i.value;
     });
 
-    if (sum > 21) {
+    setSortedCards((prev) => {
+      return [...prev, newCard];
+    });
+
+    const sum = cardsValue.reduce(reduceFunc);
+
+    if (sum > 20) {
       setFinalScore(sum);
       setGameOver(true);
+      await axios.delete(`http://localhost:8080/deck/${deck}`);
     }
   };
 
-  const sumScore = () => {
+  const sumScore = async () => {
     if (gameOver) return;
-    const sum = sortedCards.reduce(reduceFunc);
+
+    const cardsValue = sortedCards.map((i) => {
+      return i.value;
+    });
+    const sum = cardsValue.reduce(reduceFunc);
+
     setFinalScore(sum);
     setGameOver(true);
+
+    await axios.delete(`http://localhost:8080/deck/${deck}`);
+  };
+
+  const rules = () => {
+    if (finalScore === 21) {
+      return `Parabéns! Você conseguiu a pontuação perfeita de 21 pontos!`;
+    } else if (finalScore >= 18 && finalScore < 21) {
+      return `Parabéns! Você venceu com ${finalScore} pontos!`;
+    } else if (finalScore < 18) {
+      return `Você perdeu! Você conseguiu apenas ${finalScore} pontos!`;
+    } else {
+      return `Você perdeu! Você fez um total de ${finalScore} pontos!`;
+    }
   };
 
   return (
     <div className="container">
-      <div>
-        <p>{gameOver && `Fim de jogo! Sua pontuação foi de ${finalScore}`}</p>
-      </div>
       <div className="container_cards">
-        <div className="deck" onClick={() => generateNewCard()}>
-          DECK
+        <div
+          className={gameOver ? "deck end" : "deck"}
+          onClick={() => generateNewCard()}
+        >
+          <img src={pokerCard} alt="poker-deck-card" />
         </div>
-        <div className="card">
+        <div className={gameOver ? "card end" : "card"}>
           {sortedCards.map((i, index) => {
             return (
-              <div key={index}>
-                <p>{i}, </p>
+              <div className="single_card" key={index}>
+                <img src={i.image} alt={i.code} />
               </div>
             );
           })}
         </div>
       </div>
 
-      <button
-        className="button_stop"
-        onClick={() => {
-          sumScore();
-        }}
-      >
-        Parar
-      </button>
+      <div className={finalScore > 21 ? "message fail" : "message"}>
+        <p>{gameOver && rules()}</p>
+      </div>
+
+      <div className="buttons">
+        <button
+          className={gameOver ? "button_stop btn_end" : "button_stop"}
+          disabled={gameOver ? true : false}
+          onClick={() => {
+            sumScore();
+          }}
+        >
+          Parar
+        </button>
+        <button
+          className="button_reload"
+          onClick={() => {
+            window.location.reload();
+          }}
+        >
+          Reiniciar
+        </button>
+      </div>
     </div>
   );
 }
